21e80e52f848d39a236156be8e2befb68a8a00c9
agodgrab <a.lottia@o2.pl>
2019-11-08 10:49:19 +0100

    Deleted empty class UserController and comment code in UserService

--- src/main/java/com/agodgrab/carrental/service/UserService.java
+++ src/main/java/com/agodgrab/carrental/service/UserService.java
@@ -24,43 +24,4 @@ public class UserService {
         return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
     }
 
-/* --->>> TO DO AS A NEXT STEP OF DEVELOPMENT<<<<----
-
-    @Autowired
-    private UserRepository userRepository;
-
-    public User registerNewUserAccount(User user) throws EmailExistsException{
-
-
-        if (emailExists(user.getMail())){
-            throw new EmailExistsException("There is an account with that email address: " + user.getMail());
-        }
-
-        return userRepository.save(user);
-
-
-    }
-
-    private boolean emailExists(String email) {
-        return findUserByMail(email).isPresent();
-    }
-
-    public boolean confirmLogInCredentials(String email, String password) throws WrongCredentialsException{
-
-        Optional<User> user = findUserByMail(email);
-        boolean confirmation= user.isPresent() && user.get().getPassword().equals(password);
-        if(!confirmation){
-            throw new WrongCredentialsException("Wrong credentials. Check your email and password.");
-        }
-        return confirmation;
-
-        return email.equals("user")&&password.equals("userPass") || email.equals("admin")&&password.equals("adminPass");
-    }
-
-    public Optional<User> findUserByMail(String email){
-        return userRepository.findByMail(email);
-    }
-*/
-
-
 }
